0f417f4f485f4b13158ed347d2bb9c6237d59c2f
DennySPb <dennyspb@gmail.com>
2022-12-02 18:43:20 +0530

    Support Qualcomm Kryo 485 CPU variant.
    TRB: Adapt for 13
    Signed-off-by: TheHardGamer <gamelovr695@gmail.com>

--- cc/config/arm64_device.go
+++ cc/config/arm64_device.go
@@ -39,7 +39,7 @@ var (
 			"-march=armv8.2-a",
 		},
 		"armv8-2a-dotprod": []string{
-			"-march=armv8.2-a+dotprod",
+			"-march=armv8.2-a+dotprod+lse",
 		},
 		"armv9-a": []string{
 			"-march=armv9-a+nosve+dotprod",
@@ -88,6 +88,10 @@ var (
 			// kryo385 does not support dot product feature.
 			"-mcpu=cortex-a75+nodotprod",
 		},
+                "kryo485": []string{
+                        // Use cortex-a76 because kryo485 is not supported in GCC/clang.
+                        "-mcpu=cortex-a76+dotprod+lse",
+                },
 		"exynos-m1": []string{
 			"-mcpu=exynos-m1",
 		},
@@ -130,6 +134,7 @@ func init() {
 	exportedVars.ExportStringListStaticVariable("Arm64CortexA710Cflags", arm64CpuVariantCflags["cortex-a710"])
 	exportedVars.ExportStringListStaticVariable("Arm64KryoCflags", arm64CpuVariantCflags["kryo"])
 	exportedVars.ExportStringListStaticVariable("Arm64Kryo385Cflags", arm64CpuVariantCflags["kryo385"])
+	exportedVars.ExportStringListStaticVariable("Arm64Kryo485Cflags", arm64CpuVariantCflags["kryo485"])
 	exportedVars.ExportStringListStaticVariable("Arm64ExynosM1Cflags", arm64CpuVariantCflags["exynos-m1"])
 	exportedVars.ExportStringListStaticVariable("Arm64ExynosM2Cflags", arm64CpuVariantCflags["exynos-m2"])
 
@@ -156,6 +161,7 @@ var (
 		"cortex-a76": "${config.Arm64CortexA76Cflags}",
 		"kryo":       "${config.Arm64KryoCflags}",
 		"kryo385":    "${config.Arm64Kryo385Cflags}",
+		"kryo485":    "${config.Arm64Kryo485Cflags}",
 		"exynos-m1":  "${config.Arm64ExynosM1Cflags}",
 		"exynos-m2":  "${config.Arm64ExynosM2Cflags}",
 	}
